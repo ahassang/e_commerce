0ee34ae15a364029d88467cb13ef3a9ac4c3720c
Antonio <globaltailwind@protonmail.com>
2021-08-09 11:43:38 +0300

    stepper initiated @ checkout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,27 +2,33 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart} from '@material-ui/icons';
 import logo from '../../assets/commerce.png';
+import { Link,  useLocation } from 'react-router-dom';
 import useStyles from './styles';
 
 //totalItems = cart.total_items (API) which updates after item is chosen
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
+
+    
     return (
         <>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title} color="inherit">
+                    <Typography component={Link} to="/" variant='h6' className={classes.title} color="inherit">
                         <img src={logo} alt="Freedombuy.js" height='25px' className={classes.image} />
                     Freedombuy
                     </Typography>
                     <div className={classes.grow} />
+                    {location.pathname === '/' && (
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div> )}
                 </Toolbar>
 
             </AppBar>
